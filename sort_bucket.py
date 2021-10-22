ce1aae184cd0c7377b917106def8ac1ff04a18c3
kachan_v <vladimirkekb@gmail.com>
2021-10-22 09:22:08 +0300

    update

--- sort_bucket.py
+++ sort_bucket.py
@@ -1,16 +1,21 @@
-from  common.create_test_data import create_arr_for_sort
+from sort_insertion import sort_insertion
 
-def sort_insertion(arr):
-    for j in range(1, len(arr)):
-        key = arr[j]
-        i = j - 1
-        while arr[i] > key and i >= 0:
-            arr[i+1] = arr[i]
-            i -= 1
-        arr[i+1] = key
+
+# sort elements in [0,1)
+# O(n) true if some conditions are met
+def sort_bucket(arr: list, bucket_qty: int) -> list:
+    bucket = [[] for i in range(bucket_qty)]
+
+    for j in arr:  # put array elements in different buckets
+        i_b = int(10 * j)
+        bucket[i_b].append(j)
+
+    for i in range(bucket_qty):  # sort elements in any bucket by insertion sort
+        bucket[i] = sort_insertion(bucket[i])
+
+    k = 0
+    for i in range(bucket_qty):  # union the result
+        for j in range(len(bucket[i])):
+            arr[k] = bucket[i][j]
+            k += 1
     return arr
-
-arr = create_arr_for_sort()
-
-print(sort_insertion(arr))
-
